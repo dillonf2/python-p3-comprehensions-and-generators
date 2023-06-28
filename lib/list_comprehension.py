385b7e1be98e817fa203d30464639a1ab9173b62
Frank Dillon <dillonf2@outlook.com>
2023-06-28 16:57:55 -0400

    python list comprehension lab complete

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,15 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
-
+    updated_list=[]
+    for num in num_list:
+        if float(num/2)==int(num/2):
+            updated_list.append(num)
+    return updated_list
+            
 def make_exclamation(sentence_list):
-    pass
+    new_string_list=[]
+    for string in sentence_list:
+        new_string_list.append(string+"!")
+    return new_string_list
+
